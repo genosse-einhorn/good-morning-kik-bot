66c85e2b523571d4ae25006657ae226bb7b4f66e
Jonas Kümmerlin <rgcjonas@gmail.com>
2017-08-09 00:22:07 +0200

    Refactor bot: inject config, texts, schedule and kik api

---
+++ bot.js
@@ -0,0 +1,238 @@
+'use strict';
+
+let http = require('http');
+
+let DEBUG = process.env.NODE_ENV != 'production';
+function debug(msg) {
+    if (DEBUG) {
+        console.log(msg);
+    }
+}
+
+function todayIs(year, month, day) {
+    let today = new Date();
+
+    return today.getFullYear() == year
+        && today.getMonth() == month-1
+        && today.getDate() == day;
+}
+
+module.exports = class GreetingBot {
+    constructor({config, texts, KikBot, schedule, url, port}) {
+        this.config = config;
+        this.texts = texts;
+        this.KikBotClass = KikBot;
+        this.schedule = schedule;
+        this.URL = url;
+        this.PORT = port;
+    }
+
+    saveRecipient(username) {
+        if (!this.config.recipients) {
+            this.config.recipients = [];
+        }
+
+        if (!this.config.recipient_modes) {
+            this.config.recipient_modes = {};
+        }
+
+        if (this.config.recipients.indexOf(username) == -1) {
+            this.config.recipients.push(username);
+            this.config.recipient_modes[username] = 'sweet';
+            this.config.persist();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    removeRecipient(username) {
+        this.config.recipients = this.config.recipients.filter(u => u != username);
+        this.config.persist();
+    }
+
+    changeRecipientMode(user, mode) {
+        if (!this.config.recipient_modes) {
+            this.config.recipient_modes = {};
+        }
+
+        this.config.recipient_modes[user] = mode;
+        this.config.persist();
+    }
+
+    getTextRepo(user, time) {
+        let mode = 'sweet';
+        if (this.config.recipient_modes && this.config.recipient_modes[user])
+            mode = this.config.recipient_modes[user];
+
+        return this.texts[mode][time];
+    }
+
+    getMorningTextForUser(user) {
+        let greetings = this.getTextRepo(user, 'morning');
+        let maxCache = greetings.length / 2;
+
+        if (!this.config.texts_sent) this.config.texts_sent = {};
+        if (!this.config.texts_sent[user]) this.config.texts_sent[user] = [];
+
+        while (this.config.texts_sent[user].length > maxCache)
+            this.config.texts_sent[user].shift();
+
+        let available = greetings.filter(e => this.config.texts_sent[user].indexOf(e) < 0);
+
+        let text = available[Math.floor(Math.random()*available.length)];
+
+        this.config.texts_sent[user].push(text);
+        this.config.persist();
+
+        return text;
+    }
+
+    sendWithDelay(message, user, maxDelay) {
+        let delay = Math.floor(Math.random() * maxDelay);
+        debug(`Preparing to send '${message}' to ${user} in ${Math.floor(delay/1000/60)} min`);
+        setTimeout(() => {
+                this.bot.send(message, user)
+                    .then(() => {
+                        debug(`Sent '${message}' to ${user}`);
+                    });
+            }, delay);
+    }
+
+    getEveningTextForUser(user) {
+        let nighttexts = this.getTextRepo(user, 'night');
+        let maxCache = nighttexts.length / 2;
+
+        if (!this.config.night_texts_sent) this.config.night_texts_sent = {};
+        if (!this.config.night_texts_sent[user]) this.config.night_texts_sent[user] = [];
+
+        while (this.config.night_texts_sent[user].length > maxCache)
+            this.config.night_texts_sent[user].shift();
+
+        let available = nighttexts.filter(e => this.config.night_texts_sent[user].indexOf(e) < 0);
+
+        let text = available[Math.floor(Math.random()*available.length)];
+
+        this.config.night_texts_sent[user].push(text);
+        this.config.persist();
+
+        return text;
+    }
+
+    start() {
+        // Configure the bot API endpoint, details for your bot
+        this.bot = new this.KikBotClass({
+            username: this.config.username,
+            apiKey: this.config.key,
+            baseUrl: this.URL
+        });
+
+        this.bot.updateBotConfiguration();
+
+        this.bot.onTextMessage((message) => {
+            if (this.saveRecipient(message.from)) {
+                message.reply(['Congratulations! You will now receive good morning texts!',
+                        "Here's a first text to make you excited for the next morning: ",
+                        this.getMorningTextForUser(message.from)])
+                    .then(() => {
+                        debug(`Registered user ${message.from}`);
+                    });
+            } else if (message.body.match(/thank/i) || message.body.match(/^thx/i)) {
+                this.bot.send("You're welcome!", message.from)
+                    .then(() => {
+                        debug(`${message.from} thanked us: ${message.body}`);
+                    });
+            } else if (message.body.match(/evening/i) || message.body.match(/night/i)) {
+                let text = this.getEveningTextForUser(message.from);
+                this.bot.send(["Can't wait for the sunset? Here's a text for you:", text], message.from)
+                .then(() => {
+                    debug(`${message.from} couldn't wait: ${message.body}`);
+                    debug(`we sent them '${text}'`);
+                });
+            } else if (message.body.match(/^be naughty to me$/i)) {
+                this.changeRecipientMode(message.from, 'insult');
+                this.bot.send('ok, you wanted it that way', message.from)
+                .then(() => {
+                    debug(`${message.from} got put on the naughty list.`);
+                });
+            } else if (message.body.match(/^be nice to me$/i)) {
+                this.changeRecipientMode(message.from, 'sweet');
+                this.bot.send('oh no problem sweetie :)', message.from)
+                .then(() => {
+                    debug(`${message.from} is back on the nice list`);
+                });
+            } else if (message.body.match(/^leave me (the fuck )?alone$/i)) {
+                this.removeRecipient(message.from);
+                this.bot.send(['I\'m truly devastated to see you leave :(',
+                        'You can text me anytime to resume our relationship'], message.from)
+                .then(() => {
+                    debug(`${message.from} has left us :(`);
+                });
+            } else {
+                let text = this.getMorningTextForUser(message.from);
+                this.bot.send(["Can't wait until morning? Here's a text for you:", text], message.from)
+                    .then(() => {
+                        debug(`${message.from} couldn't wait: ${message.body}`);
+                        debug(`we sent them '${text}'`);
+                    });
+            }
+        });
+
+        // Set up your server and start listening
+        this.server = http
+            .createServer(this.bot.incoming())
+            .listen(this.PORT);
+
+        this.schedule.scheduleJob('0 7 * * *', () => {
+            for (let user of this.config.recipients) {
+                if (user == "sunny3964" && todayIs(2016, 12, 23)) {
+                    // exam special
+                    this.sendWithDelay([
+                        this.getMorningTextForUser(user),
+                        "I was told that today is your last exam for this year. " +
+                        "Good luck! I know you can do it."
+                    ], user, 3600*1000 /* 1h */);
+                } else if (todayIs(2017, 2, 14)) {
+                    // TODO: Make year independent
+                    this.sendWithDelay(
+                        'Hey Sunshine!\n' +
+                        'Some days, I hate being a robot. Why? because robots can not have meaningful relationships with humans :(\n' +
+                        'If I weren\'t a robot, I\'d totally ask you out for a date today.\n' +
+                        'Not sure why I am telling you this... But consider yourself loved, even if it\'s just by a robot <3 🤖\n'
+                    , user, 3600*1000 /* 1h */);
+                } else if (user == "sunny3964" && todayIs(2017, 12, 10)) {
+                    // TODO: Make year independent
+                    // TODO: Insert sweet birthday text
+                    this.sendWithDelay("Happy Birthday!", user, 3600*1000 /* 1h */);
+                } else {
+                    // business as usual
+                    this.sendWithDelay(this.getMorningTextForUser(user), user, 3600*1000 /* 1h */);
+                }
+            }
+        });
+
+        // Evening Texts
+        this.schedule.scheduleJob('0 20 * * *', () => {
+            for (let user of this.config.recipients) {
+                this.sendWithDelay(this.getEveningTextForUser(user), user, 3600*2000 /* 2h */);
+            }
+        });
+
+        // Christmas Special
+        this.schedule.scheduleJob('0 18 24 12 *', () => {
+            for (let user of this.config.recipients) {
+                this.sendWithDelay("Merry Christmas, Beautiful!", user, 30*60*1000 /* 30min */)
+            }
+        });
+
+        // Ney Year Special
+        this.schedule.scheduleJob('5 0 1 1 *', () => {
+            for (let user of this.config.recipients) {
+                this.sendWithDelay("Happy New Year, My Angel <3", user, 5*60*1000 /* 5 min */);
+            }
+        });
+
+        debug(`${this.config.username} has started, registed users: ${(this.config.recipients || []).join(', ')}`);
+    }
+
+}
--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 'use strict';
 
 let util = require('util');
-let http = require('http');
 let schedule = require('node-schedule');
 let fs = require('fs');
-let Bot  = require('@kikinteractive/kik');
+let KikBotClass  = require('@kikinteractive/kik');
+
+let OurBot = require('./bot');
 
 let PORT = process.env.PORT || 8080;
 let URL = process.env.URL || null;
@@ -50,108 +51,8 @@ function debug(msg) {
     }
 }
 
-function persistConfig() {
+config.persist = function() {
     fs.writeFileSync(__dirname + '/config.json', JSON.stringify(config, null, 4));
-}
-
-function saveRecipient(username) {
-    if (!config.recipients) {
-        config.recipients = [];
-    }
-
-    if (!config.recipient_modes) {
-        config.recipient_modes = {};
-    }
-
-    if (config.recipients.indexOf(username) == -1) {
-        config.recipients.push(username);
-        config.recipient_modes[username] = 'sweet';
-        persistConfig();
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function removeRecipient(username) {
-    config.recipients = config.recipients.filter(u => u != username);
-    persistConfig();
-}
-
-function changeRecipientMode(user, mode) {
-    if (!config.recipient_modes) {
-        config.recipient_modes = {};
-    }
-
-    config.recipient_modes[user] = mode;
-    persistConfig();
-}
-
-function getTextRepo(user, time) {
-    let mode = 'sweet';
-    if (config.recipient_modes && config.recipient_modes[user])
-        mode = config.recipient_modes[user];
-
-    return texts[mode][time];
-}
-
-function getMorningTextForUser(user) {
-    let greetings = getTextRepo(user, 'morning');
-    let maxCache = greetings.length / 2;
-
-    if (!config.textsSent) config.textsSent = {};
-    if (!config.textsSent[user]) config.textsSent[user] = [];
-
-    while (config.textsSent[user].length > maxCache)
-        config.textsSent[user].shift();
-
-    let available = greetings.filter(e => config.textsSent[user].indexOf(e) < 0);
-
-    let text = available[Math.floor(Math.random()*available.length)];
-
-    config.textsSent[user].push(text);
-    persistConfig();
-
-    return text;
-}
-
-function sendWithDelay(bot, message, user, maxDelay) {
-    let delay = Math.floor(Math.random() * maxDelay);
-    debug(`Preparing to send '${message}' to ${user} in ${Math.floor(delay/1000/60)} min`);
-    setTimeout(() => {
-            bot.send(message, user)
-                .then(() => {
-                    debug(`Sent '${message}' to ${user}`);
-                });
-        }, delay);
-}
-
-function todayIs(year, month, day) {
-    let today = new Date();
-
-    return today.getFullYear() == year
-        && today.getMonth() == month-1
-        && today.getDate() == day;
-}
-
-function getEveningTextForUser(user) {
-    let nighttexts = getTextRepo(user, 'night');
-    let maxCache = nighttexts.length / 2;
-
-    if (!config.nightTextsSent) config.nightTextsSent = {};
-    if (!config.nightTextsSent[user]) config.nightTextsSent[user] = [];
-
-    while (config.nightTextsSent[user].length > maxCache)
-        config.nightTextsSent[user].shift();
-
-    let available = nighttexts.filter(e => config.nightTextsSent[user].indexOf(e) < 0);
-
-    let text = available[Math.floor(Math.random()*available.length)];
-
-    config.nightTextsSent[user].push(text);
-    persistConfig();
-
-    return text;
 }
 
 
@@ -169,128 +70,16 @@ function startBot() {
     }
 
     // Configure the bot API endpoint, details for your bot
-    let bot = new Bot({
-        username: config.username,
-        apiKey: config.key,
-        baseUrl: URL
+    let bot = new OurBot({
+        config: config,
+        texts: texts,
+        KikBot: KikBotClass,
+        schedule: schedule,
+        url: URL,
+        port: PORT
     });
 
-    bot.updateBotConfiguration();
-
-    bot.onTextMessage((message) => {
-        if (saveRecipient(message.from)) {
-            message.reply(['Congratulations! You will now receive good morning texts!',
-                    "Here's a first text to make you excited for the next morning: ",
-                    getMorningTextForUser(message.from)])
-                .then(() => {
-                    debug(`Registered user ${message.from}`);
-                });
-        } else if (message.body.match(/thank/i) || message.body.match(/^thx/i)) {
-            bot.send("You're welcome!", message.from)
-                .then(() => {
-                    debug(`${message.from} thanked us: ${message.body}`);
-                });
-        } else if (message.body.match(/evening/i) || message.body.match(/night/i)) {
-            let text = getEveningTextForUser(message.from);
-            bot.send(["Can't wait for the sunset? Here's a text for you:", text], message.from)
-            .then(() => {
-                debug(`${message.from} couldn't wait: ${message.body}`);
-                debug(`we sent them '${text}'`);
-            });
-        } else if (message.body.match(/^be naughty to me$/i)) {
-            changeRecipientMode(message.from, 'insult');
-            bot.send('ok, you wanted it that way', message.from)
-            .then(() => {
-                debug(`${message.from} got put on the naughty list.`);
-            });
-        } else if (message.body.match(/^be nice to me$/i)) {
-            changeRecipientMode(message.from, 'sweet');
-            bot.send('oh no problem sweetie :)', message.from)
-            .then(() => {
-                debug(`${message.from} is back on the nice list`);
-            });
-        } else if (message.body.match(/^leave me (the fuck )?alone$/i)) {
-            removeRecipient(message.from);
-            bot.send(['I\'m truly devastated to see you leave :(',
-                     'You can text me anytime to resume our relationship'], message.from)
-            .then(() => {
-                debug(`${message.from} has left us :(`);
-            });
-        } else {
-            let text = getMorningTextForUser(message.from);
-            bot.send(["Can't wait until morning? Here's a text for you:", text], message.from)
-                .then(() => {
-                    debug(`${message.from} couldn't wait: ${message.body}`);
-                    debug(`we sent them '${text}'`);
-                });
-        }
-    });
-
-    bot.onStartChattingMessage((message) => {
-        bot.getUserProfile(message.from)
-            .then((user) => {
-                debug(`${message.from} started chatting!`);
-                message.reply(`Hey ${user.firstName}! Do you want to receive a `+
-                    `good morning message every day? Then reply "Yes I do"`
-                );
-            });
-    });
-
-    // Set up your server and start listening
-    let server = http
-        .createServer(bot.incoming())
-        .listen(PORT);
-
-    schedule.scheduleJob('0 7 * * *', () => {
-        for (let user of config.recipients) {
-            if (user == "sunny3964" && todayIs(2016, 12, 23)) {
-                // exam special
-                sendWithDelay(bot, [
-                    getMorningTextForUser(user),
-                    "I was told that today is your last exam for this year. " +
-                    "Good luck! I know you can do it."
-                ], user, 3600*1000 /* 1h */);
-            } else if (todayIs(2017, 2, 14)) {
-                // TODO: Make year independent
-                sendWithDelay(bot,
-                    'Hey Sunshine!\n' +
-                    'Some days, I hate being a robot. Why? because robots can not have meaningful relationships with humans :(\n' +
-                    'If I weren\'t a robot, I\'d totally ask you out for a date today.\n' +
-                    'Not sure why I am telling you this... But consider yourself loved, even if it\'s just by a robot <3 🤖\n'
-                , user, 3600*1000 /* 1h */);
-            } else if (user == "sunny3964" && todayIs(2017, 12, 10)) {
-                // TODO: Make year independent
-                // TODO: Insert sweet birthday text
-                sendWithDelay(bot, "Happy Birthday!", user, 3600*1000 /* 1h */);
-            } else {
-                // business as usual
-                sendWithDelay(bot, getMorningTextForUser(user), user, 3600*1000 /* 1h */);
-            }
-        }
-    });
-
-    // Evening Texts
-    schedule.scheduleJob('0 20 * * *', () => {
-        for (let user of config.recipients) {
-            sendWithDelay(bot, getEveningTextForUser(user), user, 3600*2000 /* 2h */);
-        }
-    });
-
-    // Christmas Special
-    schedule.scheduleJob('0 18 24 12 *', () => {
-        for (let user of config.recipients) {
-            sendWithDelay(bot, "Merry Christmas, Beautiful!", user, 30*60*1000 /* 30min */);
-        }
-    });
-
-    // Ney Year Special
-    schedule.scheduleJob('5 0 1 1 *', () => {
-        for (let user of config.recipients) {
-            sendWithDelay(bot, "Happy New Year, My Angel <3", user, 5*60*1000 /* 5 min */);
-        }
-    });
-
-    debug(`${config.username} has started, registed users: ${(config.recipients || []).join(', ')}`);
+    bot.start();
 
     if (clock) {
         console.log('Time travel activated! now skipping one hour');
